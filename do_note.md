853ea152f777e78e1d703e99bf0302ac120a233c
kangkyu <kangkyu1111@gmail.com>
2015-03-16 13:58:16 -0700

    pass index view test

--- do_note.md
+++ do_note.md
@@ -15,3 +15,8 @@ The action 'index' could not be found for MoviesController
 Missing template movies/index
 
 expected to find text "3 Movies" in ""
+
+Update the code example to also expect the page to include three movie titles: "Movie 1", "Movie 2", and "Movie 3".
+
+expected to find text "Movie 1" in "3 Movies"
+
--- spec/features/list_movies_spec.rb
+++ spec/features/list_movies_spec.rb
@@ -5,5 +5,10 @@ RSpec.feature "Viewing the list of movies", type: :feature do
   it "shows the movies" do
     visit 'http://example.com/movies'
     expect(page).to have_text("3 Movies")
+
+    expect(page).to have_text("Movie 1")
+    expect(page).to have_text("Movie 2")
+    expect(page).to have_text("Movie 3")
+
   end
 end
