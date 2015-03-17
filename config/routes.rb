2b374256f50864edf1885e09553a2f8e48e739c7
kangkyu <kangkyu1111@gmail.com>
2015-03-17 11:22:34 -0700

    refactor routes and form partial for movies edit and new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root "movies#index"
-  get '/movies', to: 'movies#index'
-  get 'movies/new', to: 'movies#new', as: 'new_movie'
-  get 'movies/:id/edit', to: "movies#edit", as: 'edit_movie'
-  get '/movies/:id', to: 'movies#show', as: 'movie'
-  patch 'movies/:id', to: 'movies#update'
-  put 'movies/:id', to: 'movies#update'
-  post 'movies', to: 'movies#create'
+  resources :movies
+  # get 'movies', to: 'movies#index'
+  # post 'movies', to: 'movies#create'
+  # get 'movies/new', to: 'movies#new', as: 'new_movie'
+  # get 'movies/:id/edit', to: "movies#edit", as: 'edit_movie'
+  # get 'movies/:id', to: 'movies#show', as: 'movie'
+  # patch 'movies/:id', to: 'movies#update'
+  # put 'movies/:id', to: 'movies#update'
 end
