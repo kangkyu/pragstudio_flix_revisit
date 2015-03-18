0f960e83d4132f3328ce70ac707005d84ae10c57
kangkyu <kangkyu1111@gmail.com>
2015-03-18 15:09:22 -0700

    refactor controllers by set_movie before_action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,15 @@
 class ReviewsController < ApplicationController
+  before_action :set_movie, only: [:index, :new, :create]
+
   def index
-    @movie = Movie.find(params[:movie_id])
     @reviews = @movie.reviews
   end
 
   def new
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new
   end
 
   def create
-    @movie = Movie.find(params[:movie_id])
     @review = @movie.reviews.new(review_params)
     if @review.save
       redirect_to movie_reviews_path(@movie), notice: "Thanks for your review!"
@@ -23,5 +22,8 @@ class ReviewsController < ApplicationController
 
   def review_params
     params.require(:review).permit(:name, :stars, :comment)
+  end
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
