caf5255980d0807fb0093bc84fe02cbb5f717fc6
kangkyu <kangkyu1111@gmail.com>
2015-03-18 13:59:40 -0700

    refactor review model validation with stars constant

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,6 +5,9 @@ class Review < ActiveRecord::Base
   validates :stars, presence: true
   validates :comment, presence: true
 
-  validates :stars, inclusion: { in: 1..5,
-  message: "must be between 1 and 5" }
+  STARS = [1,2,3,4,5]
+  validates :stars, inclusion: {
+    in: STARS,
+    message: "must be between 1 and 5"
+  }
 end
