b01668c0480e0ce7c5169e4ef3ad5213fb2cdddb
kangkyu <kangkyu1111@gmail.com>
2015-03-18 15:14:59 -0700

    edit and cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,6 @@ Rails.application.routes.draw do
 
   root "movies#index"
   resources :movies do
-    resources :reviews
+    resources :reviews, only: [:index, :create, :new]
   end
-  # get 'movies', to: 'movies#index'
-  # post 'movies', to: 'movies#create'
-  # get 'movies/new', to: 'movies#new', as: 'new_movie'
-  # get 'movies/:id/edit', to: "movies#edit", as: 'edit_movie'
-  # get 'movies/:id', to: 'movies#show', as: 'movie'
-  # patch 'movies/:id', to: 'movies#update'
-  # put 'movies/:id', to: 'movies#update'
 end
