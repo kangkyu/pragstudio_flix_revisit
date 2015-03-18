9d71f69aa99595cab5d57533291c542cdb481f54
kangkyu <kangkyu1111@gmail.com>
2015-03-18 13:48:43 -0700

    add presence validations to review model

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :movie
+
+  validates :name, presence: true
+  validates :stars, presence: true
+  validates :comment, presence: true
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -12,4 +12,23 @@ describe 'A review' do
 
     expect(review.movie).to eq(movie)
   end
+
+  it "requires a name" do
+    review = Review.new(name: "")
+    review.valid?
+
+    expect(review.errors[:name].any?).to eq(true)
+  end
+  it "requires a stars" do
+    review = Review.new(stars: "")
+    review.valid?
+
+    expect(review.errors[:stars].any?).to eq(true)
+  end
+  it "requires a comment" do
+    review = Review.new(comment: "")
+    review.valid?
+
+    expect(review.errors[:comment].any?).to eq(true)
+  end
 end
